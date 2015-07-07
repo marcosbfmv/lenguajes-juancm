230a0bd6b6b81516bb0cacda19d121e0588e34ff
datoncito <rapidclimate@gmail.com>
2015-07-06 19:40:37 -0500

    Miau

---
+++ src/collections/GeneradorDeUsuarios.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneradorDeUsuarios {
+   
+    private List<Usuario> usuarios;
+    
+    public GeneradorDeUsuarios(){
+       Usuario u1=new Usuario("juan",42,"jc@gmail.com");
+       Usuario u2=new Usuario("Ana",22,"ana@outlook.com");
+       Usuario u3=new Usuario("Pedro",33, "pedro@yahoo.com");
+       
+        usuarios=new ArrayList<Usuario>();
+        
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+    
+    
+}
